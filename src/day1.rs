da82ad914868de5b0933b4e4d3e160bd605574c5
Matheus Macabu <macabu.matheus@gmail.com>
2022-12-01 09:29:16 +0100

    Add scaffolding and day 1 solution

---
+++ src/day1.rs
@@ -0,0 +1,40 @@
+fn part_1(input: &str) -> i32 {
+    input
+        .split("\n\n")
+        .map(|backpack| {
+            backpack
+                .split("\n")
+                .map(|calorie| calorie.parse::<i32>().unwrap())
+                .fold(0, |sum, calorie| sum + calorie)
+        })
+        .max()
+        .unwrap()
+}
+
+fn part_2(input: &str) -> i32 {
+    let mut total_calories = input
+        .split("\n\n")
+        .map(|backpack| {
+            backpack
+                .split("\n")
+                .map(|calorie| calorie.parse::<i32>().unwrap())
+                .fold(0, |sum, calorie| sum + calorie)
+        })
+        .collect::<Vec<_>>();
+
+    total_calories.sort();
+
+    total_calories
+        .get(total_calories.len() - 3..total_calories.len())
+        .unwrap()
+        .into_iter()
+        .fold(0, |sum, calorie| sum + calorie)
+}
+
+pub(crate) fn run() {
+    let input = include_str!("../input/01.txt");
+
+    println!("Day 1");
+    println!("\tPart 1: {}", part_1(input));
+    println!("\tPart 2: {}", part_2(input));
+}
